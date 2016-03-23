a9f26e7fed866099d81dde4e5abe321fb14d2724
hdknr <gmail@hdknr.com>
2016-03-23 18:39:51 +0900

    ReactiveProperty: 文字入力をバインドする

--- XmWeb/HomePage.xaml.cs
+++ XmWeb/HomePage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
+
+using System.Linq;
+using Reactive.Bindings;
+using System.Runtime;
 
 namespace XmWeb
 {
@@ -10,8 +13,28 @@ namespace XmWeb
 		public HomePage ()
 		{
 			InitializeComponent ();
+
+			this.BindingContext = new ViewModel {
+				Url = "https://www.apple.com"
+			};
 		}
 
+
+		public class ViewModel
+		{
+			public string Url { get; set; }
+
+			public ReactiveProperty<string> Comment {get; private set; } 
+			= new ReactiveProperty<string>("");
+
+			public ViewModel()
+			{
+				this.Comment.Value ="....";
+				this.Comment.Subscribe( s =>{
+					System.Diagnostics.Debug.WriteLine($"{Url}: Command: {s}");
+				});
+			}
+		}
 	}
 }
 

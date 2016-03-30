e4284b32facd5b0586239343190a77620629571d
hdknr <gmail@hdknr.com>
2016-03-30 14:34:43 +0900

    フォルダーの作成

--- XmWeb/LocalPage.xaml.cs
+++ XmWeb/LocalPage.xaml.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using PCLStorage;
+using System.Reactive.Linq;
+using System.Reactive;
+using System.Reactive.Threading.Tasks;
 
 namespace XmWeb
 {
@@ -13,12 +17,27 @@ namespace XmWeb
 		{
 			InitializeComponent ();
 
+			CreateFolder ();
+
 			System.Diagnostics.Debug.WriteLine (DependencyService.Get<IBaseUrl> ().Get ());
+		
+		}
+
+		void CreateFolder()
+		{
+			var rootFolder = FileSystem.Current.LocalStorage;
+			rootFolder.CreateFolderAsync(
+				"static",CreationCollisionOption.OpenIfExists)
+				.ToObservable().Subscribe(ifolder =>{ InitHtml(ifolder.Path);});		
+		}
+
+		void InitHtml(string base_dir)
+		{
 			var html = new HtmlWebViewSource {
 				Html = @"
 <html><head><link rel='stylesheet' href='css/my.css'>
 </head><body><h1>TEST</h1><img src='img/favicon.ico'>TEST</body></html>",
-				BaseUrl = DependencyService.Get<IBaseUrl> ().Get ()
+				BaseUrl = base_dir
 			};
 			MainView.Source = html;
 		}

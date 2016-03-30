81c978227d70ffbf80afa15156bcd6cfcef5d587
hdknr <gmail@hdknr.com>
2016-03-30 15:33:59 +0900

    ネイティブのレンダラでもファイルのロードをフックできないと思われる

---
+++ Droid/NativeExWebView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer (
+	typeof (XmWeb.ExWebView), typeof (XmWeb.Droid.NativeExWebView))]
+namespace XmWeb.Droid
+{
+	public class NativeExWebView :
+		Xamarin.Forms.Platform.Android.WebViewRenderer
+	{
+		public NativeExWebView ()
+		{
+		}
+
+	}
+}
---
+++ iOS/NativeExWebView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer (
+	typeof (XmWeb.ExWebView), typeof (XmWeb.iOS.NativeExWebView))]
+namespace XmWeb.iOS
+{
+	public class NativeExWebView :
+		Xamarin.Forms.Platform.iOS.WebViewRenderer
+	{
+		public NativeExWebView ()
+		{
+		}
+	}
+}
+

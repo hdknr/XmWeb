0b92e21511b8151f8f46ad0cf517b4c8ffec9a2f
hdknr <gmail@hdknr.com>
2016-03-31 18:41:18 +0900

    Razorテンプレートでページ表示

--- XmWeb/LocalPage.xaml.cs
+++ XmWeb/LocalPage.xaml.cs
@@ -32,17 +32,39 @@ namespace XmWeb
 		}
 
 		void InitHtml(string base_dir)
-		{
-			var html = new HtmlWebViewSource {
-				Html = @"
-<html><head><link rel='stylesheet' href='css/my.css'>
-</head><body><h1>TEST</h1><img src='img/favicon.ico'>TEST</body></html>",
-				BaseUrl = base_dir
-			};
-			MainView.Source = html;
+		{	
+
+			MainView.Source =  CurrentPage(new HomePageData(), base_dir) ;
 			var class_name = MainView.GetType ().FullName;
 			System.Diagnostics.Debug.Assert (class_name == "XmWeb.ExWebView");
+
+
+			MainView.Navigating  += (object sender, WebNavigatingEventArgs e) => {
+				var u = new System.Uri(e.Url);
+				System.Diagnostics.Debug.WriteLine(
+					$"{e.NavigationEvent} : {u.Query}");
+				if( e.NavigationEvent == WebNavigationEvent.NewPage ){	
+					
+					e.Cancel = false;
+					return;
+				}		
+				MainView.Source = CurrentPage(new HomePageData(), base_dir);
+			};
 		}
+
+		public HtmlWebViewSource CurrentPage(HomePageData data, string base_dir){
+			return new HtmlWebViewSource {
+				Html = (new HoemPage{Model = data}).GenerateString(),
+				BaseUrl = base_dir,
+			};
+
+		}
+	}
+
+	public class HomePageData
+	{
+		public System.DateTime  Now  {get { return System.DateTime.Now ; }}
+		public string Title { get; set; } = "Hello";
 	}
 }
 
